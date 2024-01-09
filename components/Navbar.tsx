9eab6763035edfce102cc54f34426ec2c5141fa9
Swarnendu <swarnendumaity633@gmail.com>
2024-01-09 23:38:51 +0530

    Project

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <button className='bg-color: blue '>
+        Login
+      </button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ components/SearchComponent.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { useState } from 'react';
+
+interface SearchComponentProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchComponent: React.FC<SearchComponentProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchComponent;
