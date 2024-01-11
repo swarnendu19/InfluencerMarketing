c5dc65b276c8502854744044b98077f8be4c5450
Swarnendu <swarnendumaity633@gmail.com>
2024-01-11 22:54:26 +0530

    Project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image'
+ 
 
 export default function Home() {
   return (
-     <div>
-      
-     </div>
+      <section  className="flex flex-col md:flex-row gap-2">
+       {/* SideBar */}
+
+       <div className="flex-1 py-2">
+        <h2 className='capitalize mb-8'>
+          All products
+        </h2>
+        <section className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4">
+       
+        </section>
+       </div>
+      </section>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 function Navbar() {
   return (
-    <div>
-      <button className='bg-color: blue '>
-        Login
-      </button>
-    </div>
+     <header className='bg-gray-50 dark:bg-gray-800 py-4 sticy top-0 left-0 shadow-sm z-50 mb-8'>
+      <nav className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+       <div className='flex items-center justify-between gap-x-4'>
+        <div>
+          <Link href= '/'>
+            <Image
+            src = '/youtube(1).png'
+            width = {150}
+            height={10}
+            alt='logo'
+            className='w-[100px] md:w-auto'
+            />
+          </Link>
+        </div>
+        <div className='flex items-center space-x-2 md:space-x-8'>
+         <div className='w-16  px-4 '>
+          <Link href='/cart'>
+            <div className='relative w-full'>     
+              <Image
+               src= '/cartbag.png'
+               alt='Cart Button'
+               width={1.5}
+               height={7}
+               >
+              </Image>
+            </div>
+          </Link>
+         </div>
+        </div>
+       </div>
+      </nav>
+     </header>
   )
 }
 
