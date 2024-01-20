424d506dacc97b40bda3ecbf63129e0b8bcb2d0a
Swarnendu <swarnendumaity633@gmail.com>
2024-01-20 21:10:40 +0530

    API

--- components/TestApi.tsx
+++ components/TestApi.tsx
@@ -1,48 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+ 'use client';
 
-interface UserData {
-  Date: string; // Adjust this based on the actual structure of your user data
+import { useEffect, useState } from 'react';
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+ 
 }
 
-const YourComponent: React.FC = () => {
-  const [error, setError] = useState<boolean>(false);
-  const [users, setUsers] = useState<UserData[]>([]);
+const YourPage: React.FC = () => {
+  const [users, setUsers] = useState<Todo[]>([]); // Use the Todo interface
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null); // Specify the type for the error state
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          'http://localhost:8080/api/v1/public/randomusers?page=1&limit=10'
-        );
-        setUsers(response.data);
+        const response = await fetch('http://localhost:8080/api/v1/public/randomusers?page=1&limit=10');
+        const jsonData: Todo = await response.json(); // Use the Todo interface
+
+        console.log(jsonData);
+
+        // Assuming the API response has a 'data' property containing the user list
+        setUsers([jsonData]);
       } catch (error) {
-        console.error(error);
-        setError(true);
+        console.error('Error fetching data:', error);
+       } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  if (error) {
-    return <div>Error occurred</div>;
-  }
-
-  if (!users.length) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
-      <h1 className='text-center'> My value </h1>
+      <h1>User List</h1>
+
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
       <ul>
-        {users.map((user, index) => (
-          <li key={index}>{user.Date}</li>
-        ))}
+        {!loading &&
+          users.map((user, index) => (
+            <li key={index}>
+              {/* Render user information as needed */}
+              {user.data.data.first }
+            </li>
+          ))}
       </ul>
     </div>
   );
 };
 
-export default YourComponent;
+export default YourPage;
